2bc51f19aef9a1e55ebf483ab71e87fe3f0adf47
Tovim <jaakubi@gmail.com>
2019-12-17 16:46:23 +0100

    conf.py modified

--- conf.py
+++ conf.py
@@ -68,10 +68,10 @@ html_theme_path = get_html_theme_path()
 master_doc = "index"
 exclude_patterns = ["README.*"]
 
+from recommonmark.parser import CommonMarkParser
+
 source_suffix = [".rst", ".md"]
-source_parsers = {
-    ".md": CommonMarkParser,
-}
+source_parsers = {".md": CommonMarkParser,}
 
 default_role = "code"
 
